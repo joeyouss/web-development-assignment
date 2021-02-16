d81059fdad7c711203cb89615ccba931b2847bac
Joe <72690202+joeyouss@users.noreply.github.com>
2021-02-16 14:21:25 +0530

    Update Question_6.js
    added question 6 for assignment 3

--- WD_ASSIGNMENT/assignment_3/Question_6.js
+++ WD_ASSIGNMENT/assignment_3/Question_6.js
@@ -1,1 +1,26 @@
 
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Jyoti Bisht</title>
+</head>
+
+<body>
+    <style></style>
+    <input id="fname" type="text" name="fname" placeholder="FirstName" />
+    <input id="lname" type="text" name="lname" placeholder="LastName" />
+    <button onclick="display()">Show</button>
+    <p id="displaymsg"></p>
+    <script type="text/javascript">
+        function display {
+            var fname = document.getElementById("fname").value;
+            var lname = document.getElementById("lname").value;
+            var message = `Welcome ${fname} ${lname} to the world of web development`;
+
+            document.getElementById("displaymsg").textContent = message;
+        }
+    </script>
+</body>
+
+</html>
